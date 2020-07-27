a18b749c283b1b4015823121528219100e496f9a
Alex Biehl <alex.biehl@target.com>
2020-07-27 11:33:30 +0200

    Hardcode RTS includes to cope with unregistered builds
    git cherry-pick ef2ae81a394df573510b12b7e11bba0c931249d8

--- compiler/main/CodeOutput.hs
+++ compiler/main/CodeOutput.hs
@@ -122,26 +122,10 @@ outputC dflags filenm cmm_stream packages
        -- pushing the cmm_stream inside (c.f. nativeCodeGen)
        rawcmms <- Stream.collect cmm_stream
 
-       -- figure out which header files to #include in the generated .hc file:
-       --
-       --   * extra_includes from packages
-       --   * -#include options from the cmdline and OPTIONS pragmas
-       --   * the _stub.h file, if there is one.
-       --
-       let rts = getPackageDetails dflags rtsUnitId
-
-       let cc_injects = unlines (map mk_include (includes rts))
-           mk_include h_file =
-            case h_file of
-               '"':_{-"-} -> "#include "++h_file
-               '<':_      -> "#include "++h_file
-               _          -> "#include \""++h_file++"\""
-
        let pkg_names = map installedUnitIdString packages
-
        doOutput filenm $ \ h -> do
           hPutStr h ("/* GHC_PACKAGES " ++ unwords pkg_names ++ "\n*/\n")
-          hPutStr h cc_injects
+          hPutStr h "#include \"Stg.h\"\n"
           writeCs dflags h rawcmms
 
 {-
